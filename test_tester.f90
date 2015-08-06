662379a05dac5011d7091579524de4329f2c87c1
Peter Colberg <peter@colberg.org>
2015-08-06 17:56:13 -0400

    fix variable name conflict with module name

--- test_tester.f90
+++ test_tester.f90
@@ -6,32 +6,32 @@ program test_mt
   use tester
   implicit none
 
-  type(tester_t) :: tester
+  type(tester_t) :: test
 
-  call tester% init()
+  call test% init()
 
-  call tester% assert_equal(1, 2, fail=.true.)
+  call test% assert_equal(1, 2, fail=.true.)
 
-  call tester% assert_equal(-121, -11**2)
+  call test% assert_equal(-121, -11**2)
 
-  call tester% assert_equal([1, -10, 1899010], [1, -10, 1899010])
+  call test% assert_equal([1, -10, 1899010], [1, -10, 1899010])
 
-  call tester% assert_equal(.true., 2 > 1)
+  call test% assert_equal(.true., 2 > 1)
 
-  call tester% assert_close(1.d0, (1.d0+1.d-16))
+  call test% assert_close(1.d0, (1.d0+1.d-16))
 
-  call tester% assert_close(1.d0, (1.d0+1.d-15), fail=.true.)
+  call test% assert_close(1.d0, (1.d0+1.d-15), fail=.true.)
 
-  call tester% assert_close(1., (1.+1.e-7))
+  call test% assert_close(1., (1.+1.e-7))
 
-  call tester% assert_close(1., (1.+1.e-6), fail=.true.)
+  call test% assert_close(1., (1.+1.e-6), fail=.true.)
 
-  call tester% assert_close([1.d0, 2.d0, 3.d0], [1.d0+1.d-15, 2.d0, 3.d0], fail=.true.)
+  call test% assert_close([1.d0, 2.d0, 3.d0], [1.d0+1.d-15, 2.d0, 3.d0], fail=.true.)
 
-  call tester% assert_close([1.d0, 2.d0], [1.d0, 2.d0, 3.d0], fail=.true.)
+  call test% assert_close([1.d0, 2.d0], [1.d0, 2.d0, 3.d0], fail=.true.)
 
-  call tester% assert_close([1.d0, 2.d0], [1.d0, 2.d0])
+  call test% assert_close([1.d0, 2.d0], [1.d0, 2.d0])
 
-  call tester% print()
+  call test% print()
 
 end program test_mt
