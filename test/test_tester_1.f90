e3c5604952f390fcd3495375409174736a4f0e90
Pierre de Buyl <pdebuyl@pdebuyl.be>
2018-03-01 11:24:37 +0100

    add test cases

--- test/test_tester_1.f90
+++ test/test_tester_1.f90
@@ -6,6 +6,8 @@ program test_tester_1
   use tester
   implicit none
 
+  integer, parameter :: long_k = selected_int_kind(18)
+
   type(tester_t) :: test
 
   call test% init()
@@ -31,6 +33,20 @@ program test_tester_1
   call test% assert_close([1.d0, 2.d0], [1.d0, 2.d0, 3.d0], fail=.true.)
 
   call test% assert_close([1.d0, 2.d0], [1.d0, 2.d0])
+
+  call test% assert_positive([1., 2., 0.])
+  call test% assert_positive([1.d0, huge(1.d0)])
+
+  call test% assert_equal(1, 1)
+  call test% assert_equal(1_long_k, 1_long_k)
+  call test% assert_equal([1, -huge(1), huge(1), 0], [1, -huge(1), huge(1), 0])
+  call test% assert_equal([1_long_k, -huge(1_long_k), huge(1_long_k), 0_long_k], &
+       [1_long_k, -huge(1_long_k), huge(1_long_k), 0_long_k])
+
+  call test% assert_positive(2**30)
+  call test% assert_positive(2_long_k**62)
+  call test% assert_positive([2**30, 1, 0])
+  call test% assert_positive([2_long_k**62, 1_long_k, 0_long_k])
 
   call test% print()
 
